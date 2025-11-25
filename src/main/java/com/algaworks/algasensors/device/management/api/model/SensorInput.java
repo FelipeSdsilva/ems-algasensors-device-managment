af566d1c7502385148c0bce6b826b808166abf88
Felipe Sousa <felipe.s.silva.ext@spms.min-saude.pt>
2025-11-25 09:59:24 -0300

    Feat: Create a body for input new Sensor.

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/model/SensorInput.java
@@ -0,0 +1,18 @@
+package com.algaworks.algasensors.device.management.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SensorInput {
+
+  private String name;
+  private String ip;
+  private String location;
+  private String protocol;
+  private String model;
+
+}
