e170000a554e531abda2e96212d688236163c577
Felipe Sousa <felipe.s.silva.ext@spms.min-saude.pt>
2025-11-26 09:51:56 -0300

    Feat: Layer for business management.

---
+++ src/main/java/com/algaworks/algasensors/device/management/domain/services/SensorService.java
@@ -0,0 +1,94 @@
+package com.algaworks.algasensors.device.management.domain.services;
+
+import com.algaworks.algasensors.device.management.api.model.input.SensorInput;
+import com.algaworks.algasensors.device.management.api.model.output.SensorOutput;
+import com.algaworks.algasensors.device.management.common.IdGenerator;
+import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repositories.SensorRepository;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class SensorService {
+
+  private final SensorRepository sensorRepository;
+
+  @Transactional(readOnly = true)
+  public Page<SensorOutput> search(Pageable pageable) {
+    return sensorRepository.findAll(pageable).map(this::convertToModel);
+  }
+
+  @Transactional(readOnly = true)
+  public SensorOutput getOne(TSID sensorId) {
+    Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    return convertToModel(sensor);
+  }
+
+  @Transactional
+  public SensorOutput create(SensorInput input) {
+    Sensor sensor = Sensor.builder()
+        .id(new SensorId(IdGenerator.generateTSID()))
+        .name(input.getName())
+        .ip(input.getIp())
+        .location(input.getLocation())
+        .protocol(input.getProtocol())
+        .model(input.getModel())
+        .enable(false)
+        .build();
+
+    sensorRepository.saveAndFlush(sensor);
+
+    return convertToModel(sensor);
+  }
+
+  @Transactional
+  public SensorOutput update(TSID sensorId, SensorInput input) {
+    try {
+      Sensor sensor = sensorRepository.getReferenceById(new SensorId(sensorId));
+
+      sensor.setName(input.getName());
+      sensor.setIp(input.getIp());
+      sensor.setLocation(input.getLocation());
+      sensor.setProtocol(input.getProtocol());
+      sensor.setModel(input.getModel());
+
+      sensorRepository.saveAndFlush(sensor);
+
+      return convertToModel(sensor);
+    } catch (RuntimeException e) {
+      throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+    }
+  }
+
+  @Transactional(propagation = Propagation.SUPPORTS)
+  public void delete(TSID sensorId) {
+    try {
+      sensorRepository.deleteById(new SensorId(sensorId));
+    } catch (RuntimeException e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+  }
+
+  private SensorOutput convertToModel(Sensor sensor) {
+    return SensorOutput.builder()
+        .id(sensor.getId().getValue())
+        .name(sensor.getName())
+        .ip(sensor.getIp())
+        .location(sensor.getLocation())
+        .protocol(sensor.getProtocol())
+        .model(sensor.getModel())
+        .enable(sensor.getEnable())
+        .build();
+  }
+
+}
